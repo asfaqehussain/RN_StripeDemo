4a9957da4d566ab8d55cf705f82abbd9ccae93ae
asfaqehussain <asfaqeh@gmail.com>
2023-01-30 00:27:27 +0530

    Remove unecessary code and code fomatting

--- src/PaymentScreen/index.js
+++ src/PaymentScreen/index.js
@@ -7,9 +7,6 @@ import {
 } from '@stripe/stripe-react-native';
 
 export default PaymentScreen = () => {
-  const [card, setCard] = useState(CardFieldInput.Details | null);
-  const {confirmPayment, handleCardAction} = useStripe();
-
   // Replace localhost to your IP address if you get error
   const API_URL = 'http://localhost:8000';
 
@@ -73,27 +70,6 @@ export default PaymentScreen = () => {
 
   return (
     <View style={styles.container}>
-      {/* <CardField
-        postalCodeEnabled={false}
-        placeholder={{
-          number: '4242 4242 4242 4242',
-        }}
-        cardStyle={{
-          backgroundColor: '#FFFFFF',
-          textColor: '#000000',
-        }}
-        style={{
-          width: '100%',
-          height: 50,
-          marginVertical: 30,
-        }}
-        onCardChange={(cardDetails) => {
-          setCard(cardDetails);
-        }}
-        onFocus={(focusedField) => {
-          console.log('focusField', focusedField);
-        }}
-      /> */}
       <Text
         style={{
           fontSize: 32,
